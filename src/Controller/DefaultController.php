3e0a5924435d0b615a0fabc7dc9e449bdbf965d0
Pauline GB <pauline.gb@protonmail.com>
2021-06-01 00:02:48 +0200

    symfony10 -- Bootstrap category et program

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 // src/Controller/DefaultController.php
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +15,18 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        return $this->render(
+            'index.html.twig',
+            ['programs' => $programs,
+            'categories' => $categories]
+        );
     }
 }
